b08620a19f11b47c4af07a5447f1ab90107e5567
larrykwon <koallarry11@kaist.ac.kr>
2022-06-27 14:58:13 +0900

    first commit

---
+++ src/main/java/com/project/Project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
