d8e6bfc362a709c4df1ef6b11e61999522502b5a
larrykwon <koallarry11@kaist.ac.kr>
2022-06-30 00:37:07 +0900

    RDS 생성 및 연결 테스트 완료 - 간단한 User Entity 생성 후 테스트 - 환경(prod, dev, test, local)로 분리함. - 각자 feature 만들 때는 local에서 하고 DB 스키마에 변경 생기거나 할 때만 dev에 연결 - 연결에 필요한 변수들은 보안을 위해 환경변수로 세팅

--- src/main/java/com/project/Project/ProjectApplication.java
+++ src/main/java/com/project/Project/ProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
-
 }
