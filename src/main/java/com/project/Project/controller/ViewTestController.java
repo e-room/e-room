49cd4936e5982d26ddda7249e0183e649778fc16
swa07016 <swa07016@khu.ac.kr>
2022-07-30 13:42:51 +0900

    Feature: 로그인 페이지, 에러 페이지

--- src/main/java/com/project/Project/controller/ViewTestController.java
+++ src/main/java/com/project/Project/controller/ViewTestController.java
@@ -1,8 +1,6 @@
 package com.project.Project.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +10,18 @@ import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
-public class OauthTestController {
+public class ViewTestController {
 
     private final HttpSession httpSession;
 
     @GetMapping("/")
     public String oauthTest(Model model) {
-        return "oauth-test";
+        return "login";
+    }
+
+    @GetMapping("/error")
+    public String errorPage(Model model) {
+        return "error";
     }
 
 
