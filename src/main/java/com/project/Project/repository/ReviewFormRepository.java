8a0feeab9d0f4458159dd30c07b79e9b05eb5083
larrykwon <koallarry11@kaist.ac.kr>
2022-07-31 18:34:51 +0900

    ReviewFormRepository 작성

--- src/main/java/com/project/Project/repository/ReviewFormRepository.java
+++ src/main/java/com/project/Project/repository/ReviewFormRepository.java
@@ -1,6 +1,6 @@
 package com.project.Project.repository;
 
-import com.project.Project.domain.ReviewForm;
+import com.project.Project.domain.review.ReviewForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewFormRepository extends JpaRepository<ReviewForm, Long> {
