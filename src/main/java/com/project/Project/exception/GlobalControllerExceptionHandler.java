e45fb1ec335b1cb24cc488d4c844140b7ba2df57
larrykwon <koallarry11@kaist.ac.kr>
2022-08-01 01:07:07 +0900

    400에러(파라미터 타입이 안 맞을 경우) Error Handler 추가

--- src/main/java/com/project/Project/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/project/Project/exception/GlobalControllerExceptionHandler.java
@@ -4,25 +4,35 @@ import com.project.Project.Util.JsonResult;
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.invocation.MethodArgumentResolutionException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
 
-//    @ExceptionHandler
-//    public ResponseEntity<JsonResult> conversionFailureHandler(ConversionFailedException ex){
-//
-//        JsonResult jsonResult = JsonResult.builder()
-//                .code(HttpStatus.BAD_REQUEST)
-//                .data(ex)
-//                .message(ex.getValue() + "는 " + ex.getTargetType() + "으로 변환할 수 없습니다.").build();
-//
-//        return ResponseEntity.badRequest()
-//                .body(jsonResult);
-//    }
+    @ExceptionHandler
+    public ResponseEntity<JsonResult> conversionFailureHandler(ConversionFailedException ex){
+
+        String message = ex.getValue() + "는 " + ex.getTargetType() + "으로 변환할 수 없습니다.";
+        JsonResult jsonResult = new JsonResult(HttpStatus.BAD_REQUEST,message,ex.getClass().toString());
+
+        return ResponseEntity.badRequest()
+                .body(jsonResult);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<JsonResult> MethodArgumentType(MethodArgumentTypeMismatchException ex){
+
+        String message = "파라미터가 " + ex.getParameter() + " 타입이 아닙니다.";
+        JsonResult jsonResult = new JsonResult(HttpStatus.BAD_REQUEST,message,ex.getClass().toString());
+
+        return ResponseEntity.badRequest()
+                .body(jsonResult);
+    }
 
     @ExceptionHandler
     public ResponseEntity<JsonResult> handle (ConstraintViolationException e){
@@ -30,6 +40,6 @@ public class GlobalControllerExceptionHandler {
         jsonResult.setCode(HttpStatus.BAD_REQUEST);
         jsonResult.setMessage(e.getMessage());
         jsonResult.setData(e.getClass().toString());
-        return new ResponseEntity<>(jsonResult,HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(jsonResult);
     }
 }
