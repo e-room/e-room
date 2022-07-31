4dd739edf9a093399a94e1cc16471cc83ee97ab1
larrykwon <koallarry11@kaist.ac.kr>
2022-07-31 18:17:44 +0900

    찜 기능에 맞게 MemberRoom에서 Favorite으로 변경

--- src/main/java/com/project/Project/domain/building/Building.java
+++ src/main/java/com/project/Project/domain/building/Building.java
@@ -1,8 +1,11 @@
-package com.project.Project.domain;
+package com.project.Project.domain.building;
 
+import com.project.Project.domain.BaseEntity;
+import com.project.Project.domain.interaction.Favorite;
+import com.project.Project.domain.room.Room;
 import com.project.Project.domain.embedded.Address;
 import com.project.Project.domain.embedded.Coordinate;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -10,7 +13,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter @NoArgsConstructor @AllArgsConstructor @Builder
 @SQLDelete(sql = "UPDATE building SET deleted = true WHERE id=?")
 @Where(clause = "deleted=false")
 @Entity
@@ -30,7 +33,7 @@ import java.util.List;
                 )
         }
 )
-public class Building extends BaseEntity{
+public class Building extends BaseEntity {
 
     @Id @GeneratedValue
     private Long id;
@@ -44,7 +47,12 @@ public class Building extends BaseEntity{
     private Boolean hasElevator;
 
     @OneToMany(mappedBy = "building")
+    @Builder.Default
     private List<Room> roomList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "building")
+    @Builder.Default
+    private List<Favorite> memberList = new ArrayList<>();
 
     // TODO : 이미지 업로드 방법에 따라 추후 필드 추가. ex) S3업로드 or ec2 서버내에 업로드 등
 
--- src/main/java/com/project/Project/domain/interaction/Favorite.java
+++ src/main/java/com/project/Project/domain/interaction/Favorite.java
@@ -1,16 +1,22 @@
-package com.project.Project.domain;
+package com.project.Project.domain.interaction;
 
-import lombok.Data;
+import com.project.Project.domain.BaseEntity;
+import com.project.Project.domain.Member;
+import com.project.Project.domain.building.Building;
+import lombok.*;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 
-@Data
+/*
+찜기능용 연결 테이블
+ */
+@Getter @NoArgsConstructor @AllArgsConstructor @Builder
 @SQLDelete(sql = "UPDATE member_room SET deleted = true WHERE id=?")
 @Where(clause = "deleted=false")
 @Entity
-public class MemberRoom extends BaseEntity {
+public class Favorite extends BaseEntity {
 
     @Id @GeneratedValue
     private Long id;
@@ -21,7 +27,7 @@ public class MemberRoom extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "room_id")
-    private Room room;
+    private Building building;
 
     @PreRemove
     public void deleteHandler(){
