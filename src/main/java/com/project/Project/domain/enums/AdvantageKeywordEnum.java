2b96a2d0269e6fcc5e0b3ad03093a60a5f5a8b5f
swa07016 <swa07016@khu.ac.kr>
2022-09-01 15:25:40 +0900

    Refactor: 장단점키워드 Enum 수정

--- src/main/java/com/project/Project/domain/enums/AdvantageKeywordEnum.java
+++ src/main/java/com/project/Project/domain/enums/AdvantageKeywordEnum.java
@@ -9,14 +9,12 @@ import lombok.Getter;
 @Getter
 public enum AdvantageKeywordEnum {
 
-    NONE("없음"), PARKING("주차"),  PUBLIC_TRANSPORTATION("대중교통"),
-    PARK_WALK("공원산책"), SECURITY("치안"), SECURITY_OFFICE("경비실"),
-    BUILDING_MANAGEMENT("건물관리"), SEPARATE_TRASH("분리수거"), VENTILATION("환기"),
-    MOISTURE_PROOF("방습"), INSULATION("단열"), PET_RAISING("반려동물 키우기"),
-    INSECT_PROOF("방충"), SOUND_PROOF("방음"), ELEVATOR("엘리베이터"),
-    QUIET_TOWN("조용한 동네"), FLAT_GROUND("평지"), MART_CONVENIENCE_STORE("마트/편의점"),
-    STORE("상가"), SCHOOL_ACADEMY("학교/학원"),DAY_LIGHTING("채광");
-
+    PARKING("주차"),  PUBLIC_TRANSPORTATION("대중교통"), PARK_WALK("공원산책"), SECURITY("치안"),
+    SECURITY_OFFICE("경비실"), BUILDING_MANAGEMENT("건물관리"), SEPARATE_TRASH("분리수거"),
+    VENTILATION("환기"), MOISTURE_PROOF("방습"), INSULATION("단열"), DAY_LIGHTING("채광"),
+    PET_RAISING("반려동물 키우기"), INSECT("벌레"), FLOOR_NOISE("층간소음"),
+    ELEVATOR("엘리베이터"), TOWN_NOISE("동네소음"), HILL("언덕"),
+    MART_CONVENIENCE_STORE("마트/편의점"), STORE("상가"), SCHOOL_ACADEMY("학교/학원");
     private String description;
 
     AdvantageKeywordEnum(String description) {
