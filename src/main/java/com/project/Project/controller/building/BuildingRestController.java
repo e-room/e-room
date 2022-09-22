fd7243c61d51b28671f85f5e907ee063c5a6928f
larrykwon <koallarry11@kaist.ac.kr>
2022-09-23 00:59:52 +0900

    Add: BuildingRestController 구현

--- src/main/java/com/project/Project/controller/building/BuildingRestController.java
+++ src/main/java/com/project/Project/controller/building/BuildingRestController.java
@@ -1,16 +1,32 @@
 package com.project.Project.controller.building;
 
 import com.project.Project.controller.building.dto.BuildingResponseDto;
+import com.project.Project.domain.building.Building;
+import com.project.Project.domain.embedded.Address;
+import com.project.Project.domain.embedded.Coordinate;
+import com.project.Project.domain.review.Review;
+import com.project.Project.domain.review.ReviewForm;
+import com.project.Project.repository.projection.building.OnlyBuildingIdAndCoord;
+import com.project.Project.service.BuildingService;
+import com.project.Project.service.ReviewService;
 import com.project.Project.validator.ExistBuilding;
+import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Validated
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/building")
 public class BuildingRestController {
+
+    private final BuildingService buildingService;
+
+    private final ReviewService reviewService;
 
     /*
     when: 3.0.1
@@ -22,7 +38,11 @@ public class BuildingRestController {
      */
     @GetMapping("/marking")
     public List<BuildingResponseDto.BuildingCountResponse> getBuildingMarker() {
-        return null;
+        List<OnlyBuildingIdAndCoord> buildingList = this.buildingService.getAllBuildingsIdAndCoord();
+        return buildingList.stream().map((building) -> BuildingResponseDto.BuildingCountResponse.builder()
+                .buildingId(building.getId())
+                .coordinateDto(Coordinate.toCoordinateDto(building.getCoordinate()))
+                .build()).collect(Collectors.toList());
     }
 
     /*
@@ -31,8 +51,25 @@ public class BuildingRestController {
     return: 해당하는 건물 list
      */
     @GetMapping("/")
-    public List<BuildingResponseDto.BuildingListResponse> getBuildingList(@RequestParam List<@ExistBuilding Long> buildingId) {
-        return null;
+    public List<BuildingResponseDto.BuildingListResponse> getBuildingList(@RequestParam List<@ExistBuilding Long> buildingIds) {
+        List<Building> buildingList = this.buildingService.getBuildingListByBuildingIds(buildingIds);
+
+        return buildingList.stream().map(
+                (building) -> {
+                    List<Review> reviewListByBuilding = this.reviewService.getReviewListByBuildingId(building.getId());
+                    Integer reviewCnt = reviewListByBuilding.size();
+
+                    return BuildingResponseDto.BuildingListResponse.builder()
+                            .buildingId(building.getId())
+                            .name(building.getBuildingName())
+                            .address(Address.toAddressDto(building.getAddress()).toString())
+                            .isDirectDeal(false)
+                            .reviewCnt(Long.valueOf(reviewCnt))
+                            .scoreAvg(building.getBuildingSummary().getAvgScore())
+                            .bestCategory(building.getBuildingSummary().getBestCategory())
+                            .build();
+                }
+        ).collect(Collectors.toList());
     }
 
     /*
@@ -42,7 +79,8 @@ public class BuildingRestController {
      */
     @GetMapping("/{buildingId}")
     public BuildingResponseDto.BuildingResponse getBuilding(@PathVariable("buildingId") @ExistBuilding Long buildingId) {
-        return null;
+        Building building = this.buildingService.getBuildingByBuildingId(buildingId);
+        return building.toBuildingResponse();
     }
 
     /* 8.1
@@ -54,6 +92,8 @@ public class BuildingRestController {
      */
     @GetMapping("/search")
     public List<BuildingResponseDto.BuildingResponse> searchBuilding(@RequestParam("params") String params) {
-        return null;
+        List<Building> buildingList = this.buildingService.getBuildingBySearch(params);
+        return buildingList.stream().parallel()
+                .map((building)-> building.toBuildingResponse()).collect(Collectors.toList());
     }
 }
