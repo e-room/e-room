ffe8a9dc81bf5122390e7a9d5eba6ff2675a3424
larrykwon <koallarry11@kaist.ac.kr>
2022-09-30 01:51:43 +0900

    Fix: make CustomException and make BuildingException extends it

--- src/main/java/com/project/Project/exception/building/BuildingException.java
+++ src/main/java/com/project/Project/exception/building/BuildingException.java
@@ -1,17 +1,14 @@
 package com.project.Project.exception.building;
 
+import com.project.Project.exception.CustomException;
 import com.project.Project.exception.ErrorCode;
 
-public class BuildingException extends RuntimeException{
-    private ErrorCode errorCode;
-
-    public BuildingException(String message, ErrorCode errorCode){
-        super(message);
-        this.errorCode = errorCode;
+public class BuildingException extends CustomException {
+    public BuildingException(String message, ErrorCode errorCode) {
+        super(message, errorCode);
     }
 
-    public BuildingException(ErrorCode errorCode){
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
+    public BuildingException(ErrorCode errorCode) {
+        super(errorCode);
     }
 }
