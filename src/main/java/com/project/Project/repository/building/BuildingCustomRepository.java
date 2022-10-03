91d388e28a2485dc9848929a807e31c47a5a8f24
larrykwon <koallarry11@kaist.ac.kr>
2022-10-04 01:42:55 +0900

    Add: create BuildingCustomRepository

--- src/main/java/com/project/Project/repository/building/BuildingCustomRepository.java
+++ src/main/java/com/project/Project/repository/building/BuildingCustomRepository.java
@@ -3,10 +3,13 @@ package com.project.Project.repository.building;
 import com.project.Project.domain.building.Building;
 import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
 import java.util.function.Function;
 
 public interface BuildingCustomRepository {
-    Function<String,JPAQuery<Building>> searchBuildingsQuery ();
+    Function<String,JPAQuery<Building>> searchBuildingsQuery ( Long cursorId, Pageable pageable);
+
+    List<Building> searchBuildings(String params, Long cursorId, Pageable pageable);
 }
