dcec693e65b68e4b3abf6e89316a15ebc109d40c
larrykwon <koallarry11@kaist.ac.kr>
2022-10-10 23:42:36 +0900

    Add: implement method for findByAddress

--- src/main/java/com/project/Project/service/impl/BuildingServiceImpl.java
+++ src/main/java/com/project/Project/service/impl/BuildingServiceImpl.java
@@ -1,6 +1,10 @@
 package com.project.Project.service.impl;
 
+import com.project.Project.Util.BuildingGenerator;
+import com.project.Project.Util.KakaoAddressAPI;
 import com.project.Project.domain.building.Building;
+import com.project.Project.domain.embedded.Address;
+import com.project.Project.domain.embedded.Coordinate;
 import com.project.Project.repository.building.BuildingCustomRepository;
 import com.project.Project.repository.building.BuildingRepository;
 import com.project.Project.repository.ReviewRepository;
@@ -8,9 +12,11 @@ import com.project.Project.repository.RoomRepository;
 import com.project.Project.repository.projection.building.OnlyBuildingIdAndCoord;
 import com.project.Project.service.BuildingService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,7 +28,8 @@ public class BuildingServiceImpl implements BuildingService {
     private final BuildingCustomRepository buildingCustomRepo;
     private final RoomRepository roomRepository;
     private final ReviewRepository reviewRepository;
-
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
+    private final WebClient webclient;
 
     public List<OnlyBuildingIdAndCoord> getAllBuildingsIdAndCoord(){
         return buildingRepository.findBy(OnlyBuildingIdAndCoord.class);
@@ -45,7 +52,7 @@ public class BuildingServiceImpl implements BuildingService {
     }
 
     @Override
-    public Optional<Building> findByAddress(String address) {
+    public Optional<Building> findByAddress(Address address) {
         return buildingRepository.findByAddress(address);
     }
 
