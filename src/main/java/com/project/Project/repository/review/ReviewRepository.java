15543bdc2712280097da37ed0cc1da8ba8a3f616
larrykwon <koallarry11@kaist.ac.kr>
2022-10-23 00:40:42 +0900

    Add: implement repository for review and reviewCategory

--- src/main/java/com/project/Project/repository/review/ReviewRepository.java
+++ src/main/java/com/project/Project/repository/review/ReviewRepository.java
@@ -3,8 +3,12 @@ package com.project.Project.repository.review;
 import com.project.Project.domain.review.Review;
 import com.project.Project.domain.room.Room;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
@@ -14,5 +18,14 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByIdInOrderByCreatedAtDesc(List<Long> ids, Pageable pageable);
 
     List<Review> findByIdInAndIdLessThanOrderByCreatedAtDesc(List<Long> ids, Long id, Pageable pageable);
+
+    @EntityGraph(value = "Review.withRoomAndBuilding", type = EntityGraph.EntityGraphType.FETCH)
+    @Query("select review from Review review where review.room.building.id = :id")
+    List<Review> findReviewsWithRoomAndBuilding(Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @EntityGraph(value = "Review.withRoomAndBuilding", type = EntityGraph.EntityGraphType.FETCH)
+    @Query("select review from Review review where review.room.building.id = :id")
+    List<Review> findReviewsWithRoomAndBuildingAndLock(Long id);
 
 }
