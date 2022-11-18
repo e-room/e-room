bee8a391491a2ac9311fc8ed33e806c300d0787b
swa07016 <swa07016@naver.com>
2022-11-18 16:03:38 +0900

    feat: ReviewLikeRestController method signature

--- src/main/java/com/project/Project/controller/interaction/controller/ReviewLikeRestController.java
+++ src/main/java/com/project/Project/controller/interaction/controller/ReviewLikeRestController.java
@@ -1,7 +1,22 @@
 package com.project.Project.controller.interaction.controller;
 
+import com.project.Project.auth.AuthUser;
+import com.project.Project.controller.interaction.dto.ReviewLikeResponseDto;
+import com.project.Project.domain.Member;
+import com.project.Project.service.interaction.ReviewLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ReviewLikeRestController {
+    private final ReviewLikeService reviewLikeService;
+
+    @PostMapping("/building/room/review/like/{reviewId}")
+    public ResponseEntity<ReviewLikeResponseDto.ReviewLikeUpdateResponse> updateReviewLike(@PathVariable("reviewId") Long reviewId, @AuthUser Member member) {
+        return null;
+    }
 }
--- src/main/java/com/project/Project/controller/interaction/dto/ReviewLikeRequestDto.java
+++ src/main/java/com/project/Project/controller/interaction/dto/ReviewLikeRequestDto.java
@@ -1,4 +1,5 @@
 package com.project.Project.controller.interaction.dto;
 
 public class ReviewLikeRequestDto {
+
 }
