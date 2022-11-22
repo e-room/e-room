3db5bd6f00c7b2b2c230ebdfa043badbde6fe283
swa07016 <swa07016@naver.com>
2022-11-22 23:42:30 +0900

    fix: test 패키지 구조 변경

---
+++ src/test/java/com/project/Project/unit/repository/RepositoryTestConfig.java
@@ -0,0 +1,23 @@
+package com.project.Project.unit.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@TestConfiguration
+public class RepositoryTestConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+
+}
+
