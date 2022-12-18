c084ae981d24fa7bb460422d47279c2cdacc8fb2
larrykwon <koallarry11@kaist.ac.kr>
2022-12-18 19:51:19 +0900

    Fix: Compile Error

--- src/main/java/com/project/Project/controller/interaction/controller/FavoriteRestController.java
+++ src/main/java/com/project/Project/controller/interaction/controller/FavoriteRestController.java
@@ -1,31 +1,26 @@
 package com.project.Project.controller.interaction.controller;
 
-import com.project.Project.Util.QueryDslUtil;
 import com.project.Project.auth.AuthUser;
-import com.project.Project.util.component.QueryDslUtil;
-import com.project.Project.controller.CursorDto;
 import com.project.Project.controller.building.dto.BuildingResponseDto;
 import com.project.Project.controller.interaction.dto.FavoriteResponseDto;
 import com.project.Project.domain.Member;
 import com.project.Project.domain.building.Building;
-import com.project.Project.domain.enums.MemberRole;
 import com.project.Project.exception.ErrorCode;
-import com.project.Project.exception.building.BuildingException;
 import com.project.Project.exception.interaction.FavoriteException;
 import com.project.Project.serializer.building.BuildingSerializer;
 import com.project.Project.service.FavoriteService;
+import com.project.Project.util.component.QueryDslUtil;
 import com.project.Project.validator.ExistBuilding;
-
 import com.project.Project.validator.interaction.FavoriteExistValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,7 +36,7 @@ public class FavoriteRestController {
 
     @GetMapping("/member/favorite")
     public ResponseEntity<Slice<BuildingResponseDto.BuildingListResponse>> getFavoriteBuildingList(@RequestParam(required = false) List<Double> cursorIds, @PageableDefault(size = 10, sort = "id", page = 0, direction = Sort.Direction.DESC) Pageable pageable, @AuthUser Member member) {
-        if(cursorIds == null) cursorIds = new ArrayList<>();
+        if (cursorIds == null) cursorIds = new ArrayList<>();
         List<Building> buildingList = favoriteService.getBuildingListByMember(member, cursorIds, pageable);
         List<BuildingResponseDto.BuildingListResponse> buildingListResponse = buildingList.stream().map((building) -> BuildingSerializer.toBuildingListResponse(building)).collect(Collectors.toList());
         return ResponseEntity.ok(QueryDslUtil.toSlice(buildingListResponse, pageable));
@@ -59,7 +54,7 @@ public class FavoriteRestController {
 
     @DeleteMapping("/member/favorite/{buildingId}")
     public ResponseEntity<FavoriteResponseDto.FavoriteDeleteResponse> deleteFavoriteBuilding(@PathVariable("buildingId") @ExistBuilding Long buildingId, @AuthUser Member member) {
-        if(!favoriteExistValidator.isValid(member, buildingId))
+        if (!favoriteExistValidator.isValid(member, buildingId))
             throw new FavoriteException(ErrorCode.FAVORITE_NOT_FOUND);
 
         Long deletedFavoriteId = favoriteService.deleteFavoriteBuilding(buildingId, member);
