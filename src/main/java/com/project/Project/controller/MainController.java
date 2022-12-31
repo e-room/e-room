0660228507e3a2e8ad8f990e6f4ead154290c2af
larrykwon <koallarry11@kaist.ac.kr>
2022-12-31 23:41:17 +0900

    Add: resolve error

--- src/main/java/com/project/Project/controller/MainController.java
+++ src/main/java/com/project/Project/controller/MainController.java
@@ -6,16 +6,14 @@ import com.project.Project.exception.building.BuildingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
 public class MainController {
 
     @GetMapping("/health")
-    public String oauthTest(@RequestParam("test") MultipartFile test) {
+    public String oauthTest() {
         return "I'm healthy";
     }
 
