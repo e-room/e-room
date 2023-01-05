2979882a39be99194481fd1c373a07fa0b705a8e
swa07016 <swa07016@naver.com>
2023-01-05 17:43:06 +0900

    feat: 리뷰 좋아요 응답 DTO 변경

--- src/main/java/com/project/Project/controller/interaction/controller/ReviewLikeRestController.java
+++ src/main/java/com/project/Project/controller/interaction/controller/ReviewLikeRestController.java
@@ -2,10 +2,18 @@ package com.project.Project.controller.interaction.controller;
 
 import com.project.Project.auth.AuthUser;
 import com.project.Project.controller.interaction.dto.ReviewLikeResponseDto;
+import com.project.Project.domain.interaction.ReviewLike;
 import com.project.Project.domain.member.Member;
 import com.project.Project.serializer.interaction.ReviewLikeSerializer;
 import com.project.Project.service.interaction.ReviewLikeService;
 import com.project.Project.validator.ExistReview;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,10 +31,14 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class ReviewLikeRestController {
     private final ReviewLikeService reviewLikeService;
-
+    @Operation(summary = "리뷰 좋아요", description = "리뷰 좋아요/취소")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ReviewLikeResponseDto.ReviewLikeUpdateResponse.class)))
+    })
+    @Parameter(name = "reviewId", description = "좋아요/취소하고자하는 리뷰의 id", example = "1021")
     @PutMapping("/building/room/review/like/{reviewId}")
     public ResponseEntity<ReviewLikeResponseDto.ReviewLikeUpdateResponse> updateReviewLike(@PathVariable("reviewId") @ExistReview Long reviewId, @AuthUser Member member) {
-        Long updatedReviewLikeId = reviewLikeService.updateReviewLike(reviewId, member);
-        return ResponseEntity.ok(ReviewLikeSerializer.toReviewLikeUpdateResponse(updatedReviewLikeId));
+        ReviewLike updatedReviewLike = reviewLikeService.updateReviewLike(reviewId, member);
+        return ResponseEntity.ok(ReviewLikeSerializer.toReviewLikeUpdateResponse(updatedReviewLike));
     }
 }
