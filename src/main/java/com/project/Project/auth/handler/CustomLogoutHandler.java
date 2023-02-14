af7d37fd82e2ac1f6fb49b574dba42e3fdca91d6
swa07016 <swa07016@naver.com>
2023-02-14 22:57:54 +0900

    fix: logout 시 리다이렉트를 시키지 않고, 200 응답을 하도록 수정

--- src/main/java/com/project/Project/auth/handler/CustomLogoutHandler.java
+++ src/main/java/com/project/Project/auth/handler/CustomLogoutHandler.java
@@ -6,16 +6,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static com.project.Project.auth.repository.OAuth2AuthorizationRequestBasedOnCookieRepository.IS_LOCAL;
+
 @Component
 public class CustomLogoutHandler implements LogoutHandler {
-    private ResponseCookie createDeleteTokenCookie(String name) {
+    private ResponseCookie createDeleteTokenCookie(String name, Boolean isLocal) {
         return ResponseCookie.from(name, null)
                 .secure(true)
                 .httpOnly(true)
-                .domain(".e-room.app")
+                .domain(isLocal ? "localhost" : ".e-room.app")
                 .path("/")
                 .sameSite("None")
                 .maxAge(0)
@@ -24,8 +27,12 @@ public class CustomLogoutHandler implements LogoutHandler {
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        ResponseCookie accessTokenCookie = createDeleteTokenCookie("accessToken");
-        ResponseCookie refreshTokenCookie = createDeleteTokenCookie("refreshToken");
+        Boolean isLocal = CookieUtil.getCookie(request, IS_LOCAL)
+                .map(Cookie::getValue)
+                .map(Boolean::parseBoolean).orElse(false);
+
+        ResponseCookie accessTokenCookie = createDeleteTokenCookie("accessToken", isLocal);
+        ResponseCookie refreshTokenCookie = createDeleteTokenCookie("refreshToken", isLocal);
 
         response.addHeader("Set-Cookie", accessTokenCookie.toString());
         response.addHeader("Set-Cookie", refreshTokenCookie.toString());
