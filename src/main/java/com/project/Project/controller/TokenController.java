6413b868d833845123feb9feca4a8945483f24d1
swa07016 <swa07016@naver.com>
2023-02-19 23:17:51 +0900

    feat: GET /token/valid API에 refreshToken으로 재발급해주는 로직 추가

--- src/main/java/com/project/Project/controller/TokenController.java
+++ src/main/java/com/project/Project/controller/TokenController.java
@@ -1,9 +1,12 @@
 package com.project.Project.controller;
 
 import com.project.Project.auth.AuthUser;
+import com.project.Project.auth.authentication.JwtAuthentication;
 import com.project.Project.auth.dto.MemberDto;
 import com.project.Project.auth.dto.Token;
 import com.project.Project.auth.dto.UidDto;
+import com.project.Project.auth.exception.JwtException;
+import com.project.Project.auth.handler.JWTFailureHandler;
 import com.project.Project.auth.service.TokenService;
 import com.project.Project.domain.enums.AuthProviderType;
 import com.project.Project.domain.member.Member;
@@ -18,21 +21,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+
+import static com.project.Project.auth.repository.OAuth2AuthorizationRequestBasedOnCookieRepository.IS_LOCAL;
 
 @Tag(name = "Token API", description = "토큰 만료, 재발급, 검증")
 @RequiredArgsConstructor
 @RestController
 public class TokenController {
     private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
+    private final JWTFailureHandler failureHandler;
 
     // 토큰이 만료되었을 경우 사용자에게 안내해주기
     @GetMapping("/token/expired")
@@ -67,31 +79,57 @@ public class TokenController {
         throw new RuntimeException();
     }
 
+    private String getCookieValue(HttpServletRequest req, String cookieName) {
+        if (req.getCookies() == null) return null;
+        return Arrays.stream(req.getCookies())
+                .filter(c -> c.getName().equals(cookieName))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse(null);
+    }
+
+    private void postAuthenticate(HttpServletRequest request, HttpServletResponse response, Authentication authenticationResult) {
+        Boolean isLocal = CookieUtil.getCookie(request, IS_LOCAL)
+                .map(Cookie::getValue)
+                .map(Boolean::parseBoolean).orElse(false);
+        JwtAuthentication jwtAuthenticationResult = (JwtAuthentication) authenticationResult;
+        ResponseCookie accessTokenCookie = CookieUtil.createAccessTokenCookie(jwtAuthenticationResult.getToken().getAccessToken(), isLocal);
+        ResponseCookie refreshTokenCookie = CookieUtil.createRefreshTokenCookie(jwtAuthenticationResult.getToken().getRefreshToken(), isLocal);
+        response.addHeader("Set-Cookie", accessTokenCookie.toString());
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+        System.out.println("refersh: "+refreshTokenCookie);
+    }
+
     @Operation(summary = "토큰 검증", description = "토큰 유효성 검사 API")
     @GetMapping("/token/valid")
     @Parameters({
             @Parameter(name = "member", hidden = true)
     })
-    public ResponseEntity<TokenResponseDto.TokenValidDto> validUser(HttpServletRequest req, HttpServletResponse response) {
-        Boolean existsAccessToken = false;
+    public ResponseEntity<TokenResponseDto.TokenValidDto> validUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Boolean isValid = true;
-        Cookie[] cookies = req.getCookies();
-        if(cookies == null) isValid = false;
-        else {
-            for(Cookie c : cookies) {
-                if(c.getName().equals("accessToken")) {
-                    existsAccessToken = true;
-                    try {
-                        TokenService.JwtCode jwtCode = tokenService.verifyToken(c.getValue());
-                        if(jwtCode != TokenService.JwtCode.ACCESS) isValid = false;
-                    } catch (Exception e) {
-                        isValid = false;
-                    }
-                    break;
-                }
+        String accessToken = getCookieValue(request, "accessToken");
+        String refreshToken = getCookieValue(request, "refreshToken");
+        if(accessToken != null) {
+            try {
+                TokenService.JwtCode jwtCode = tokenService.verifyToken(accessToken);
+                if(jwtCode != TokenService.JwtCode.ACCESS) isValid = false;
+            } catch (Exception e) {
+                isValid = false;
             }
         }
-        if(!existsAccessToken) isValid = false;
+        else if(refreshToken != null) {
+            try {
+                Token jwtToken = new Token(accessToken, refreshToken);
+                JwtAuthentication authenticationRequest = new JwtAuthentication(jwtToken, request, response);
+                JwtAuthentication authenticationResult = (JwtAuthentication) this.authenticationManager.authenticate(authenticationRequest);
+                SecurityContextHolder.getContext().setAuthentication(authenticationResult);
+                postAuthenticate(request, response, authenticationResult);
+            } catch (JwtException ex) {
+                SecurityContextHolder.clearContext();
+                this.failureHandler.onAuthenticationFailure(request, response, ex);
+            }
+        }
+        else isValid = false;
         return ResponseEntity.ok(TokenResponseDto.TokenValidDto.builder().isValid(isValid).build());
     }
 
