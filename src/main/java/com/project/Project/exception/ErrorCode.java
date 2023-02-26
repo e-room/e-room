1f995ddfee67b8b4fd41312ae267633375c2e4dd
LarryKwon <65128957+LarryKwon@users.noreply.github.com>
2023-02-26 15:28:20 +0900

    Issue/110-image-nickname (#118)
    * [issue/110] Add: add anonymousStatus to building images
    
    * [issue/93] Fix: commit missed

--- src/main/java/com/project/Project/exception/ErrorCode.java
+++ src/main/java/com/project/Project/exception/ErrorCode.java
@@ -31,6 +31,9 @@ public enum ErrorCode {
     REVIEW_DUPLICATED(HttpStatus.BAD_REQUEST, "하나의 건물에는 하나의 리뷰만 작성할 수 있습니다."),
     REVIEW_ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "해당 리뷰에 접근할 수 없습니다."),
 
+    //ReviewImage
+    NO_REVIEW_IN_REVIEW_IMAGE(HttpStatus.BAD_REQUEST, "리뷰 이미지에 리뷰가 할당되어있지 않습니다."),
+
     //etc
     UUID_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당하는 UUID가 존재하지 않습니다."),
     EVENT_LISTENER_INJECTION(HttpStatus.INTERNAL_SERVER_ERROR, "의존성 주입에 실패했습니다."),
