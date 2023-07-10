917fe5d65d7227658ed9e94a8f6dc409a9ad58dc
swa07016 <swa07016@naver.com>
2023-07-10 16:39:43 +0900

    refactor: ProfileImageServiceImpl

--- src/main/java/com/project/Project/service/member/impl/ProfileImageServiceImpl.java
+++ src/main/java/com/project/Project/service/member/impl/ProfileImageServiceImpl.java
@@ -9,17 +9,20 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Random;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ProfileImageServiceImpl implements ProfileImageService {
 
     private final ProfileImageRepository profileImageRepository;
+    private Random random = new Random();
 
     @Override
     public ProfileImage random() {
         Long totalCount = profileImageRepository.count();
-        int randomIndex = (int) (Math.random() * totalCount);
+        int randomIndex = random.nextInt(totalCount.intValue());
         Page<ProfileImage> profileImageUrlPage = profileImageRepository.findAll(PageRequest.of(randomIndex, 1));
         return profileImageUrlPage.getContent().get(0);
     }
