03daf7b3043c48416ccaaadc1a5e2b129e4c98e1
swa07016 <swa07016@naver.com>
2023-08-13 23:18:01 +0900

    fix: import statement

--- src/main/java/com/project/Project/common/serializer/checklist/ChecklistSerializer.java
+++ src/main/java/com/project/Project/common/serializer/checklist/ChecklistSerializer.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
