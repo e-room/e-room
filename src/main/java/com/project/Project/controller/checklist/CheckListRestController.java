b4a81f1018d7da79ef52e41e853b2f1047940a2b
swa07016 <swa07016@naver.com>
2023-08-13 16:47:31 +0900

    docs: POST /checklists 스웨거 명세 작성

--- src/main/java/com/project/Project/controller/checklist/CheckListRestController.java
+++ src/main/java/com/project/Project/controller/checklist/CheckListRestController.java
@@ -8,11 +8,20 @@ import com.project.Project.domain.checklist.CheckList;
 import com.project.Project.domain.checklist.CheckListImage;
 import com.project.Project.domain.member.Member;
 import com.project.Project.service.checklist.ChecklistService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "Checklist API", description = "체크리스트 등록, 조회, 삭제")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/checklists")
@@ -33,6 +42,13 @@ public class CheckListRestController {
         return ResponseEntity.ok(ChecklistSerializer.toChecklistImageDto(checkListImage));
     }
 
+    @Operation(summary = "발품기록 단계1: 집정보 입력", description = "체크리스트 생성 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = ChecklistResponseDto.ChecklistCreateDto.class)))
+    })
+    @Parameters({
+            @Parameter(name = "member", hidden = true)
+    })
     @PostMapping("/checklists")
     public ResponseEntity<ChecklistResponseDto.ChecklistCreateDto> createChecklist(@RequestBody ChecklistRequestDto.ChecklistCreateDto request, @AuthUser Member member) {
         CheckList savedCheckList = checklistService.create(request, member);
